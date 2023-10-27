ae284ab623e3283f294c06bd7eb55587199115bd
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2023-10-27 22:57:58 +0100

    added first model

--- hrmsAPI/backend/models.py
+++ hrmsAPI/backend/models.py
@@ -1,9 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
+class Order(Base):
+    __tablename__ = 'Orders'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    table_id = Column(Integer)
+    complete = Column(Boolean)
+
 engine = create_engine('sqlite:///mydatabase.db')  # вот тут нужен урл нашей дб
 
-# вот тут будут модели
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base.metadata.create_all(engine)
