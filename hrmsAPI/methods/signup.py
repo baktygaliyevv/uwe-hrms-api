b6b99de1ab71491a98275a974aa46f497792c15f
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2023-10-31 14:56:46 +0000

    fixes

--- hrmsAPI/methods/signup.py
+++ hrmsAPI/methods/signup.py
@@ -4,28 +4,21 @@ from sqlalchemy import create_engine
 import json
 from django.http import JsonResponse
 from settings import Session
+from .utils.responses import error, ok
 
 def signup(request):
     data = json.loads(request.body.decode("utf-8"))
     phone = data.get("phone")
     password = data.get("password")
 
-    # Create a new session
-    session = Session()
+    with Session() as session:
+        # Check if the user already exists
+        existing_user = session.query(User).filter_by(phone=phone).first()
+        if existing_user:
+            return error('This user already exists')
+        
+        new_user = User(phone=phone, password=password, role='client')
 
-    # Check if the user already exists
-    existing_user = session.query(User).filter_by(phone=phone).first()
-    if existing_user:
-        session.close()
-        return JsonResponse({'status': 'Error', 'message': 'This user already exists.'}, status=400)
-
-    # Create a new user with the 'client' role by default
-    new_user = User(phone=phone, password=password, role='client')
-
-    session.add(new_user)
-    session.commit()
+        session.add(new_user)
     
-    # Close the session
-    session.close()
-
-    return JsonResponse({'status': 'Ok'}, status=200)
+    return ok
