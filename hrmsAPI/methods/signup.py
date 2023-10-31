d83db6d926ce7e5d75a24ca1944c90777ef19829
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2023-10-31 16:19:31 +0000

    hashed the password

--- hrmsAPI/methods/signup.py
+++ hrmsAPI/methods/signup.py
@@ -1,15 +1,17 @@
-from sqlalchemy.orm import sessionmaker
 from .entities.user import User
-from sqlalchemy import create_engine
 import json
-from django.http import JsonResponse
 from settings import Session
 from .utils.responses import error, ok
+import hashlib
+import secrets 
 
 def signup(request):
     data = json.loads(request.body.decode("utf-8"))
     phone = data.get("phone")
     password = data.get("password")
+
+    salt = secrets.token_hex(16)
+    hashed_password = hashlib.sha1((password + salt).encode('utf-8')).hexdigest()
 
     with Session() as session:
         # Check if the user already exists
@@ -17,7 +19,7 @@ def signup(request):
         if existing_user:
             return error('This user already exists')
         
-        new_user = User(phone=phone, password=password, role='client')
+        new_user = User(phone=phone, password=hashed_password, salt=salt, role='client')
 
         session.add(new_user)
     
