69b04bb0fb5620a965aa7cf923e198c7de01cd4c
KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>
2024-03-23 12:50:39 +0200

    delivery

---
+++ hrmsAPI/methods/delivery/views.py
@@ -0,0 +1,29 @@
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ...models import Deliveries, DeliveryMenu
+from .serializer import DeliverySerializer, DeliveryMenuSerializer
+
+class GetAllDelivery(generics.ListAPIView):
+    def get(self, request, *args, **kwargs):
+        queryset = Deliveries.objects.all()
+        serializer_class = DeliverySerializer(queryset, many=True)
+        return Response({
+            'status': 'Ok',
+            'payload': serializer_class.data
+        })
+
+class AddDelivery(generics.CreateAPIView):  
+    queryset = Deliveries.objects.all()
+    serializer_class = DeliverySerializer
+
+class EditDelivery(generics.UpdateAPIView):
+    queryset = Deliveries.objects.all()
+    serializer_class = DeliverySerializer
+    lookup_field = 'id'
+
+class DeleteDelivery(generics.DestroyAPIView):
+    queryset = Deliveries.objects.all()
+    serializer_class = DeliverySerializer
+    lookup_field = 'id'
