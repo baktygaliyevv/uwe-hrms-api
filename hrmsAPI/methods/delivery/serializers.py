881645e19103f1016b320434ab11d9fb0efc1666
Nuraly <kracken787@gmail.com>
2024-03-24 15:27:57 +0000

    delivery serializers updated

--- hrmsAPI/methods/delivery/serializers.py
+++ hrmsAPI/methods/delivery/serializers.py
@@ -19,7 +19,22 @@ class DeliverySerializer(serializers.ModelSerializer):
         model = Deliveries
         fields = ['id', 'user', 'restaurant', 'promocode', 'address', 'created_at', 'status', 'items']
 
+    # def get_items(self, obj):
+    #     delivery_menus = DeliveryMenu.objects.filter(delivery=obj)
+    #     menus = [dm.menu for dm in delivery_menus]
+    #     return MenuSerializer(menus, many=True).data
     def get_items(self, obj):
-        delivery_menus = DeliveryMenu.objects.filter(delivery=obj)
-        menus = [dm.menu for dm in delivery_menus]
-        return MenuSerializer(menus, many=True).data
+        delivery_menus = DeliveryMenu.objects.filter(delivery=obj).select_related('menu')
+        items_data = []
+        for delivery_menu in delivery_menus:
+            item_data = {
+                'item': MenuSerializer(delivery_menu.menu).data,
+                'quantity': delivery_menu.quantity
+            }
+            items_data.append(item_data)
+        return items_data
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+        return super().create(validated_data)
