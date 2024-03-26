3787d81bcc7b94cbb057de06d5a5421eabe50025
KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>
2024-03-26 15:56:22 +0200

    deliveries_methods_add_edit_delete

--- hrmsAPI/methods/delivery/serializers.py
+++ hrmsAPI/methods/delivery/serializers.py
@@ -1,28 +1,50 @@
 from rest_framework import serializers
+from ...models import Deliveries, DeliveryMenu, Users, Restaurants, Promocodes
+from ..menu.serializers import MenuSerializer
 from ..users.serializers import UserSerializer
 from ..promocodes.serializers import PromocodeSerializer
-from ...models import Deliveries, DeliveryMenu
 from ..restaurants.serializers import RestaurantSerializer
-from ..menu.serializers import MenuSerializer
+#from ..users.views import Users
 
 class DeliveryMenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = DeliveryMenu
         fields = ['id', 'delivery', 'menu', 'quantity']
 
-class DeliverySerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    promocode = PromocodeSerializer(read_only=True)
-    restaurant = RestaurantSerializer(read_only=True)    
-    items = serializers.SerializerMethodField()
+# class DeliverySerializer(serializers.ModelSerializer):
+#     user = serializers.PrimaryKeyRelatedField(queryset=Users.objects.all())
+#     promocode = serializers.PrimaryKeyRelatedField(queryset=Promocodes.objects.all())
+#     restaurant = serializers.PrimaryKeyRelatedField(queryset=Restaurants.objects.all())    
+#     items = serializers.SerializerMethodField()
+#     class Meta:
+#         model = Deliveries
+#         fields = ['id', 'user', 'restaurant', 'promocode', 'address', 'created_at', 'status', 'items']
+class DeliveryCreateUpdateSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=Users.objects.all())
+    promocode = serializers.PrimaryKeyRelatedField(queryset=Promocodes.objects.all())
+    restaurant = serializers.PrimaryKeyRelatedField(queryset=Restaurants.objects.all())
+    items = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Deliveries
-        fields = ['id', 'user', 'restaurant', 'promocode', 'address', 'created_at', 'status', 'items']
+        fields = ['id', 'user', 'restaurant', 'promocode', 'address', 'created_at', 'status', 'items'] 
 
-    # def get_items(self, obj):
-    #     delivery_menus = DeliveryMenu.objects.filter(delivery=obj)
-    #     menus = [dm.menu for dm in delivery_menus]
-    #     return MenuSerializer(menus, many=True).data
+    def get_items(self, obj):
+        delivery_menus = DeliveryMenu.objects.filter(delivery=obj).select_related('menu')
+        items_data = []
+        for delivery_menu in delivery_menus:
+            item_data = {
+                'item': MenuSerializer(delivery_menu.menu).data,
+                'quantity': delivery_menu.quantity
+            }
+            items_data.append(item_data)
+        return items_data  
+    
+class DeliveryReadSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    promocode = PromocodeSerializer(read_only=True)
+    restaurant = RestaurantSerializer(read_only=True)
+    items = serializers.SerializerMethodField(read_only=True)
+
     def get_items(self, obj):
         delivery_menus = DeliveryMenu.objects.filter(delivery=obj).select_related('menu')
         items_data = []
@@ -38,3 +60,7 @@ class DeliverySerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         validated_data['user'] = user
         return super().create(validated_data)
+    
+    class Meta:
+        model = Deliveries
+        fields = ['id', 'user', 'restaurant', 'promocode', 'address', 'created_at', 'status', 'items']
--- hrmsAPI/methods/delivery/views.py
+++ hrmsAPI/methods/delivery/views.py
@@ -1,13 +1,14 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from ...models import Deliveries, DeliveryMenu
-from .serializers import DeliveryMenuSerializer, DeliverySerializer
+from ...models import Deliveries
+from .serializers import DeliveryReadSerializer, DeliveryCreateUpdateSerializer
+from rest_framework.mixins import RetrieveModelMixin
 
 class GetDeliveries(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         queryset = Deliveries.objects.all()
-        serializer_class = DeliverySerializer(queryset, many=True)
+        serializer_class = DeliveryReadSerializer(queryset, many=True)
         return Response({
             'status': 'Ok',
             'payload': serializer_class.data
@@ -15,14 +16,17 @@ class GetDeliveries(generics.ListAPIView):
 
 class AddDelivery(generics.CreateAPIView):
     queryset = Deliveries.objects.all()
-    serializer_class = DeliverySerializer
+    serializer_class = DeliveryCreateUpdateSerializer
 
-class EditDelivery(generics.UpdateAPIView):
+class EditDelivery(RetrieveModelMixin, generics.UpdateAPIView):
     queryset = Deliveries.objects.all()
-    serializer_class = DeliverySerializer
+    serializer_class = DeliveryCreateUpdateSerializer
     lookup_field = 'id'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 class DeleteDelivery(generics.DestroyAPIView):
     queryset = Deliveries.objects.all()
-    serializer_class = DeliverySerializer
+    serializer_class = DeliveryCreateUpdateSerializer
     lookup_field = 'id'
