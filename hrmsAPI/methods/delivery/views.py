f71908688eacdba894f1c45e1283ee8915298c9d
KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>
2024-04-05 19:43:58 +0300

    Added /deliveries/client path

--- hrmsAPI/methods/delivery/views.py
+++ hrmsAPI/methods/delivery/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from ...models import Deliveries
-from .serializers import DeliveryReadSerializer, DeliveryCreateUpdateSerializer, DeliveryUISerializer
+from ...models import Deliveries, UserTokens
+from .serializers import DeliveryReadSerializer, DeliveryCreateUpdateSerializer, DeliveryReadClientSerializer, DeliveryCreateUpdateClientSerilizer
 from ..users.serializers import UserSerializer
 from rest_framework.mixins import RetrieveModelMixin
 
@@ -19,6 +20,7 @@ class AddDelivery(generics.CreateAPIView):
     queryset = Deliveries.objects.all()
     serializer_class = DeliveryCreateUpdateSerializer
 
+<<<<<<< Updated upstream
 class GetDeliveryByUserId(generics.ListAPIView):
     serializer_class = DeliveryReadSerializer
 
@@ -33,10 +35,24 @@ class GetDeliveryByUserId(generics.ListAPIView):
             'status': 'Ok',
             'payload': serializer_class.data
         })
+=======
+class EditDelivery(RetrieveModelMixin, generics.UpdateAPIView):
+    queryset = Deliveries.objects.all()
+    serializer_class = DeliveryCreateUpdateSerializer
+    lookup_field = 'id'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+class DeleteDelivery(generics.DestroyAPIView):
+    queryset = Deliveries.objects.all()
+    serializer_class = DeliveryCreateUpdateSerializer
+    lookup_field = 'id'
+>>>>>>> Stashed changes
 
 class AddUiClientDelivery(generics.CreateAPIView):
     queryset = Deliveries.objects.all()
-    serializer_class = DeliveryUISerializer
+    serializer_class = DeliveryCreateUpdateClientSerilizer
 
     def create(self, request, *args, **kwargs):
         user_data = {
@@ -62,16 +78,26 @@ class AddUiClientDelivery(generics.CreateAPIView):
         else:
             return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class GetAddClientDeliveries(ListCreateAPIView):
+    def get_serializer_class(self):
+        if self.request.method == 'list':
+            return DeliveryReadClientSerializer
+        return DeliveryCreateUpdateClientSerilizer
 
-class EditDelivery(RetrieveModelMixin, generics.UpdateAPIView):
-    queryset = Deliveries.objects.all()
-    serializer_class = DeliveryCreateUpdateSerializer
-    lookup_field = 'id'
+    def list(self, request):
+        obj = UserTokens.objects.get(token=request.COOKIES['token'])
+        user_id = UserSerializer(obj.user).data.get('id')
+        queryset = Deliveries.objects.filter(user_id = user_id)
+        serializer_class = DeliveryReadClientSerializer(queryset, many=True)
+        return Response({
+            'status': 'Ok',
+            'payload': serializer_class.data
+        })
+    
+    def create(self, request, *args, **kwargs):
+            serializer = DeliveryCreateUpdateClientSerilizer(data=request.data, context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            order = serializer.save()
+            order_serializer = DeliveryReadSerializer(order)
+            return Response({'status': 'Ok', 'payload': order_serializer.data}, status=status.HTTP_201_CREATED)
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-class DeleteDelivery(generics.DestroyAPIView):
-    queryset = Deliveries.objects.all()
-    serializer_class = DeliveryCreateUpdateSerializer
-    lookup_field = 'id'
