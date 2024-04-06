fb948f0d1dac8b1933f3fd5fe213f98839b04617
Ilyas Utenov <117728333+0dreadnought@users.noreply.github.com>
2024-04-06 23:54:47 +0500

    now works (#86)

--- hrmsAPI/urls.py
+++ hrmsAPI/urls.py
@@ -21,7 +21,7 @@ from .methods.users.views import UserListCreateAPIView, EditDeleteUser
 from .methods.menu.views import GetMenuItems, AddMenuItem, EditMenuItem, DeleteMenuItem, AddMenuCategory, AddMenuProduct, DeleteMenuProduct, GetMenuCategories
 from .methods.products.views import GetAddProducts, DeleteProduct, EditProduct
 from .methods.promocodes.views import GetAllPromocodes, AddPromocode, DeletePromocode, GetSpecificPromocode
-from .methods.tables.views import EditDeleteTable, GetAllTables, AddTable
+from .methods.tables.views import EditDeleteTable, GetAddTables
 from .methods.orders.views import GetAddOrder, GetAddClientOrder, EditDeleteOrder, AddOrderMenu, EditDeleteOrderMenu
 from .methods.restaurants.views import GetRestaurant,DeleteRestaurant
 from .methods.auth.views import AuthView, AuthLoginView, AuthSignupView, AuthVerifyView
@@ -57,8 +57,7 @@ urlpatterns = [
     path(f'{API_BASE_URL}promocodes/<id>', GetSpecificPromocode.as_view(), name='get-specific-promocode'),
 
     #tables
-    path(f'{API_BASE_URL}tables', GetAllTables.as_view(), name='get-tables'),
-    path(f'{API_BASE_URL}tables', AddTable.as_view(), name='add-tables'),
+    path(f'{API_BASE_URL}tables', GetAddTables.as_view(), name='get-add-tables'),
     path(f'{API_BASE_URL}tables/<int:id>', EditDeleteTable.as_view(), name='edit-delete-table'),
 
     #auth
